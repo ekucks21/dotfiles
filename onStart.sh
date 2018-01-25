627add3438b9584276fb2dab1cfbc8fa830457f7
Eric Kucks <ekucks@ata-llc.com>
2018-01-25 16:56:26 -0500

    onStart.sh requires less setup beforehand to run

--- onStart.sh
+++ onStart.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $DIR/.xkb ~/.xkb
+
+if [ ! -d ~/sb/Space2Ctrl ]; then
+    cd ~/sb
+    git clone git@github.com:r0adrunner/Space2Ctrl.git
+    cd ~/sb/Space2Ctrl
+    make
+    sudo make install
+fi
+
 xkbcomp -I$HOME/.xkb ~/.xkb/keymap/xkbtest $DISPLAY
+s2cctl stop
 s2cctl start
